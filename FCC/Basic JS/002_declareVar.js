0553b902e74ecb004a2e198785e5864dd4dc8601
shahalias <shah94.alias@gmail.com>
2023-10-09 01:04:11 +0800

    Initial commit

---
+++ FCC/Basic JS/002_declareVar.js
@@ -0,0 +1,9 @@
+// Declare JavaScript Variables
+
+/*
+Data Type in JS
+undefined, null, boolean, string, symbol, bigint, number,object
+*/
+
+// T - You should declare myName with the var keyword, ending with a semicolon
+var myName;
---
+++ FCC/Basic JS/003_storeValue.js
@@ -0,0 +1,8 @@
+// Storing Values with the Assignment Operator
+
+// store a value in a variable with the assignment operator (=)
+// myVariable = 5;
+
+// a should have a value of 7.
+var a ;
+a = 7 ;

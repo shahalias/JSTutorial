ce786f81312fbd77fc0d4f0e7820c1c45374cf1a
shahalias <shah94.alias@gmail.com>
2023-10-09 18:51:05 +0800

    20231009_001

---
+++ FCC/Basic JS/004_assignVarToVar.js
@@ -0,0 +1,11 @@
+// Assigning the Value of One Variable to Another
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+// b should have a value of 7.
+// a should be assigned to b with =.
+b = a ;
